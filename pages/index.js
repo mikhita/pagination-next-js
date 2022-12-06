a416bc162fddeb6de4e438a3cad851f14a76b2b0
mikhita <mikheilispiriani@yahoo.com>
2022-12-06 16:31:37 +0400

    I get posts from rest api and move it to ui

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,36 @@ const Home = () => {
 
   useEffect(() => {
     const getPosts = async () => {
-      const { data: res } = axios.get(
+      const { data: res } = await axios.get(
         "https://jsonplaceholder.typicode.com/posts"
       );
       setPosts(res);
     };
     getPosts();
   }, []);
+
   return (
     <div className="container">
-      <table className="table"></table>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts?.map((post) => (
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>
+                <button className="btn btn-danger btn-sm">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
